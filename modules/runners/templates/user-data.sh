16d4c04c3f30e9890830eec7e4df6a517ee553f0
Connor Bell (Makeshift) <git@connor-bell.com>
2022-04-21 12:54:29 +0100

    Remove some conflicting userscript stuff for our specific setup

--- modules/runners/templates/user-data.sh
+++ modules/runners/templates/user-data.sh
@@ -1,16 +1,6 @@
 #!/bin/bash -e
-exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&1
 
 ${pre_install}
-
-yum update -y
-
-# Install docker
-amazon-linux-extras install docker
-service docker start
-usermod -a -G docker ec2-user
-
-yum install -y amazon-cloudwatch-agent curl jq git
 
 user_name=ec2-user
 
